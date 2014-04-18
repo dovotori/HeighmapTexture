28400afdeb5fb6ada4e9460f5b8c96725ba8cf27
Peh4 <pierrealain@rsf.org>
2014-04-18 11:10:57 +0200

    récupération des modifications
    faites directement sur le serveur

---
+++ tr-index2014.php
@@ -0,0 +1,32 @@
+<?php include "tr-header.php" ?>
+<h5>Türkiye 180 Ülke İçinde 154. Sırada Kaldı</h5>
+<p id="intro" >Uluslar arası Sınır Tanımayan Gazeteciler (RSF) örgütü, 2014 Dünya Basın Özgürlüğü Sıralaması’nda Türkiye’yi, 180 ülke içerisinde 154. Sırada gösterdi.</p>
+<div id="more">
+	<p>Basın özgürlüğünü savunan Paris merkezli uluslar arası örgüt, bugün yayınladığı İndeks’inde Türkiye’yi, 2013 sonunda 60 civarında medya çalışanını cezaevinde tutan, Gezi eylemleriyle 153 habercinin polis şiddetine uğradığı ve editoryal bağımsızlığın ağır saldırı altında olduğu bir ülke olarak tanıttı.</p>
+	<p>Basın özgürlüğü durumu en iyi ülkeler Finlandiya (1), Hollanda (2) ve Norveç (3), sıralamanın ilk üç sırasında yer alırken Suriye (177), Türkmenistan (178), Kuzey Kore (179) ve Eritre (180) son sıraları paylaştılar.</p>
+	<h6>AB’de gerileyenler de var</h6>
+	<p>Çoğu gerileme gösteren AB ülkelerinden Fransa (39) iki sıra, Romanya (45) üç sıra, Bulgaristan (100) 13 sıra, &nbsp;Yunanistan (99) ise 15 sıra arkaya düştü.</p>
+	<h6>10 yılda 56 sıra geriledik</h6>
+	<p>Gürcistan (84) 16 sıra ilerlerken Ermenistan (78) dört sıra, Azerbaycan (160) dört sıra geriledi. İran (173) bir sıra ilerlerken Irak, geçen yıla göre üç sıra geride 153. Sırada gösterildi.</p>
+	<p>Türkiye, yeni savaştan çıkan Irak ile Gambiya arasında kendisine yer buldu. RSF, bir önceki sıralamada Türkiye’nin yerini “TMK ve yargı paranoyası” ile açıklarken bu yıl “tutuklu gazeteciler, Gezi’de habercilere saldırı ve dış editoryal müdahalelere” vurgu yaptı.</p>
+	<p>Türkiye, geçen yıl 179 ülke içerisinde 154. Sıradaydı; bu yıl bir fazla ülkeyle aynı sırada kaldı. Türkiye basın özgürlüğünde 2005 yılından beri kan kaybediyor. 2005’te 98. Sırada olan Türkiye, 2012 yılında 148. Sıraya kadar geriledi (2005’te 98, 2006’da 100, 2007’de 101, 2008’de 103, 2009’da 123, 2010’da 138. Sıra).</p>
+	<p>Büyüyen diplomatik ve ekonomik etkisiyle Türkiye’nin özellikle de “Arap Baharı”nı yaşamış rejimler için kendisini bölgesel bir demokrasi modeli olarak sergilediğini kaydeden RSF, basın bildirisinde Türkiye ile ilgili şu tespitleri paylaştı:</p>
+	<h6>Yol ayırımındaki Türkiye</h6>
+	<p>İslami muhafazakar Ak Parti’nin 10 yıllık iktidarında, ordunun siyaset ve medya alanındaki etkisi kayboldu ve Kemalist ideolojiye bağlı kimi tabuların etkisi kırıldı. Ancak bunların yerini yeni kırmızı çizgiler alma eğilimindedir.</p>
+	<p>Çekinik reformlara rağmen, yargı pratiği büyük ölçüde baskıcı özünü koruyor. Tutuklu medya profesyonellerinin sayısı askeri rejimden beri hiç olmadığı kadar yüksek: Türkiye, 2013 sonunda 60 kadar tutuklu gazetecisiyle dünyanın en büyük cezaevlerinden biri haline geliyordu.</p>
+	<p>Tedbir tutukluluklarını sınırlandırmaya dönük talimatlara karşın, tutuklular yargılanmadan sıklıkla aylarını hatta yıllarını hapiste geçirmek zorunda kalıyorlar.</p>
+	<p>Ağır çatışmalardan sonra Recep Tayyip Erdoğan hükümeti ile PKK güçleri arasındaki barış müzakerelerinin açılması büyük umutlar doğurdu. Yetkililer demokratik reform sözü verdi. Bu reformlar acildir.</p>
+	<p>Tutuklu ve yargılanan gazetecilerin çoğu, baskıcı yıllardan miras Terörle Mücadele mevzuatının bedelini ödüyor. Ceza Kanunu’ndaki 20 kadar madde de, özgürlükleri hedef alan bu düzenlemeleri tamamlıyor.</p>
+	<p>Mevzuatın ötesinde, Kürt sorununun barışçıl çözümü konusundaki bir uzlaşı, paranoyak ve baskıcı izler taşıyan yargı kültürünü nihayet geliştirmeyi sağlayacaktır.</p>
+	<p>2013 yazında yaşanan, benzeri olmayan protestolar, Türkiye toplumunun özgürlüğe olan ihtiyacını gösterdi. Olay, güvenlik kuvvetlerinde demokratik kültür eksikliğini ve büyük medya gruplarının hükümetle bağı olan iş insanlarının elinde toplanmasının çoğulculuk bakımından yol açtığı tehlikeyi de ortaya koydu.</p>
+	<h6>Gezi gazeteciler için mayınlı bölge</h6>
+	<p>153 gazeteci yaralandı, 39’u gözaltına alındı: Mayıs- Eylül 2013 döneminde gazeteciler, hükümet karşıtı protestolarının şiddetle bastırılmasının bedelini ziyadesiyle ödediler. Kimi zaman eylemcilerin hedefi oldularsa da sistemli saldırılar güvenlik güçlerinden geldi.</p>
+	<p>Bu şiddet patlaması resmi söylemlerinin ve hükümete yakın medya kuruluşlarının yaydığı histerik havada gerçekleşti: Köşe yazarları eleştirildi; sosyal medya kullanıcıları ve uluslar arası medya çalışanları rejimi yıkmaya dönük uluslar arası bir komplonun ajanları, teröristler olarak gösterildiler.</p>
+	<p>Oto-sansür, habere ağırlık veren televizyon kanalları İstanbul’u sarsan şiddetli çatışmaları haber olarak vermeye yanaşmamalarıyla, tüm boyutlarıyla ortadaydı. İflah olmaz gazetecilere kapı gösterildi: En az 14’ü işten çıkarıldı; 22’si istifa etmeyi tercih etti. Eylemleri daha yakından aktara televizyon kanallarına ise astronomik para cezaları verildi.</p>
+	<h6>Rusya ve eski Sovyet ülkeleri</h6>
+	<p>Soçi Olimpiyatları’nın Kremlin ile kendini daha iyi ifade eden sivil toplum arasındaki bilek güreşini gizlemeye yetmediğini bildiren RSF, Putin’in yeniden Kremlin’in başına geçtiği 2012’den beri hakaret suçuna yeniden hapis öngörüldüğünü, İnternet sitelerinden “kara liste” oluşturulduğunu, haberleşme özgürlüğünün “geleneksel değerler” saikiyle sınırlandırıldığını, “inançlara hakaret” veya homoseksüellik propagandası” gibi suçlamalarla düşüncenin sınırlandırılması yoluna gidildiğini bildirdi.</p>
+	<h6>“Zenginlikleri oranında dokunulmaz”</h6>
+	<p>RSF, Türkmenistan, Özbekistan, Kazakistan ve Azerbaycan’dan da haberleşme özgürlüğünü hedef almalarına karşın “uluslar arası toplum”un sahip oldukları doğal kaynak rezervleri nedeniyle baskı yapmaya yanaşmadığı ülkeler olarak söz etti.</p>
+</div>
+													
+<?php include "tr-footer.php" ?>
